cd86a14fa69c3101e03bddc3a7fc915a32a1c7c8
zberisha <zarif.berisha@live.com>
2024-12-16 08:02:00 +0100

    muted by default an addded on hover event

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("page loaded...");
+document.addEventListener('DOMContentLoaded', () => {
+    const videoContainer = document.querySelector('.video-player');
+    const video = document.querySelector('.video');
+    const overlay = document.querySelector('.overlay');
+
+    videoContainer.addEventListener('mouseenter', playVideo);
+    videoContainer.addEventListener('mouseleave', pauseVideo);
+    
+    videoContainer.addEventListener('touchstart', playVideo);
+    videoContainer.addEventListener('touchend', pauseVideo);
+
+
+
+    function playVideo() {
+        video.play();
+        overlay.style.opacity = '0';
+    
+    }
+
+    function pauseVideo() {
+        video.pause();
+        overlay.style.opacity = '1';
+    }
+
+
+})
